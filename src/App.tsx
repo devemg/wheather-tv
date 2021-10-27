6a25d1e1b54d8ffe866e7ebaeb9a8632560c9bd8
Emely Garcia <garciam.emm@gmail.com>
2021-10-27 07:14:14 -0600

    using state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moon from './img-states/moon.png';
 import './App.css';
 
 function App() {
+  const [datetime] = useState({
+    year: '2021',
+    month: 'Octubre',
+    day: '26',
+    time: '11:00',
+    dayOfWeek: 'Domingo',
+    isAM: true,
+    is24Hs: false
+  });
+  const [language] = useState('es');
+
   return (
     <div className="background">
       <div className="image-container">
           <img className="image" src={moon} alt="image" /> 
-          <p className="time">11:00 PM</p>
+          <p className="time">{getTime(datetime.time, datetime.isAM, datetime.is24Hs)}</p>
       </div>
       <div className="info-container">
         <div className="info">
@@ -16,13 +27,28 @@ function App() {
           <h3>21°C</h3>
         </div>
         <div className="date">
-          <p>Domingo</p>
-          <p>24 de Octubre</p>
-          <p>2021</p>
+          <p>{datetime.dayOfWeek}</p>
+          <p>{getDateByLanguage(datetime.day,datetime.month,language)}</p>
+          <p>{datetime.year}</p>
         </div>
       </div>
     </div>
   );
+}
+
+function getDateByLanguage(day: string, month: string, language: string){
+  switch(language) {
+    case 'es': return `${day} de ${month}`;
+    case 'en': return `${month} ${day}th`;
+    default: return `${day} de ${month}`;
+  }
+}
+
+function getTime(time: string, isAM: boolean, is24Hs: boolean) {
+  if (!is24Hs) {
+    return isAM ? time+' AM' : time +' PM';
+  }
+  return time;
 }
 
 export default App;
