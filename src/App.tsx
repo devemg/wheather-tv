4231a336e9a2b3e00c671ec854afd8fa7f7403c2
Emely Garcia <garciam.emm@gmail.com>
2021-10-27 06:54:03 -0600

    add styles of screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import moon from './img-states/moon.png';
 import './App.css';
 
 function App() {
   return (
     <div className="background">
       <div className="image-container">
-          <img className="image" src={logo} alt="image" /> 
-          <p>11:00 PM</p>
+          <img className="image" src={moon} alt="image" /> 
+          <p className="time">11:00 PM</p>
       </div>
       <div className="info-container">
-        <h1>Ciudad de Guatemala</h1>
-        <h2>Mayormente Despejado</h2>
-        <h3>21°C</h3>
-        <p>Domingo</p>
-        <p>24 de Octubre</p>
+        <div className="info">
+          <h1>Ciudad de Guatemala</h1>
+          <h2>Mayormente Despejado</h2>
+          <h3>21°C</h3>
+        </div>
+        <div className="date">
+          <p>Domingo</p>
+          <p>24 de Octubre</p>
+          <p>2021</p>
+        </div>
       </div>
     </div>
   );
