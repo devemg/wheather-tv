efe354e83d6bfdc208f8d4f53228639f7d699fd8
Emely Garcia <garciam.emm@gmail.com>
2021-11-02 13:44:23 -0600

    working on dynamic background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import moon from './img-states/moon.png';
+import RAINY from './img-states/rainy-day.png';
+import CLOUDS from './img-states/rain.png';
 import './App.css';
 import moment from 'moment';
 import 'moment/locale/es';
@@ -16,7 +18,8 @@ function App() {
   const [language] = useState('es');
   const [wheather, setWheater] = useState({
     location: { city: '', country: '', region: '' },
-    wheatherStatus: '',
+    wheatherDescription: '',
+    wheatherCode: 0,
     celsius: ''
   });
   //const [isLoading, setLoading] = useState(true);
@@ -24,13 +27,9 @@ function App() {
   useEffect(() => {
     moment.locale(language);
     fetch(`http://ip-api.com/json?lang=${language}`).then(res => res.json()).then((response) => {
-      console.log(response);
       fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${response.lat}&lon=${response.lon}&units=metric&lang=${language}&APPID=ee054a87257ae80863574e6c33b0ab77`)
       .then(res1 => res1.json()).then((responseWheather) => {
-      console.log(responseWheather);
       //setLoading(false);
-      //set location and wheather
-
       //set date and time
       const date = moment();
       setDate({
@@ -40,30 +39,30 @@ function App() {
         dayOfWeek: date.format('dddd'),
         isMorning: false
       });
-
       setTime({ hours: date.hour(), minutes: date.minute(), isAM: isAM(date.hour())});
-      //console.log(generateGreetings())
+      // set wehather
       setWheater({
         location: { city: response.city, country: response.country, region: response.regionName },
         celsius: `${responseWheather.main.temp} °C`,
-        wheatherStatus: responseWheather.weather.map((element:any)=>element.description).join(',')
+        wheatherDescription: responseWheather.weather[0].description,
+        wheatherCode: responseWheather.weather[0].id
       });
     })
     })
   }, [language]); // se ejecuta una sola vez al renderizar el componente.
  
   return (
-    <div className="background">
+    <div className="background background-day">
       <div className="image-container">
-          <img className="image" src={moon} alt="image" /> 
+          <img className="image" src={getImageByStatus(wheather.wheatherCode)} alt="image" /> 
           <p className="time">{time.hours}:{time.minutes} {time.isAM? 'AM' : 'PM'}</p>
       </div>
       <div className="info-container">
         <div className="info">
-          <h1>{  wheather.location.city }</h1>
+          <h1>{  wheather.location.city == wheather.location.country ? wheather.location.city : wheather.location.country }</h1>
           <h2>{  !wheather.location.region.includes(wheather.location.city) ? wheather.location.region : '' }</h2>
           <h2>{  !wheather.location.country.includes(wheather.location.city) ? wheather.location.country : ''}</h2>
-          <h3>{ wheather.wheatherStatus }</h3>
+          <h3>{ wheather.wheatherDescription }</h3>
           <h3>{ wheather.celsius}</h3>
         </div>
         <div className="date">
@@ -88,6 +87,26 @@ function isAM(currentHour:number){
   return currentHour >= 0 && currentHour < 12;
 }
 
+function getImageByStatus(code: number) {
+  switch (code) {
+    // CLOUDS
+    case 801:
+    case 802:
+      return RAINY;
+    case 803:
+    case 804: 
+      return CLOUDS;
+    // CLEAR
+    case 800: 
+      return CLOUDS;
+    default: 
+    // SNOW
+    if (code >= 600 && code <=622) {
+      return CLOUDS;
+    }
+  }
+  return moon;
+}
 /*
 
 function generateGreetings(){
