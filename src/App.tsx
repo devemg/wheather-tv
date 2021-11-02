48036d088700c8ea84aa67f0e2160cfa7eddf18d
Emely Garcia <garciam.emm@gmail.com>
2021-11-02 11:14:05 -0600

    dynamic data

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,69 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import moon from './img-states/moon.png';
 import './App.css';
+import moment from 'moment'
 
 function App() {
-  const [datetime] = useState({
-    year: '2021',
-    month: 'Octubre',
-    day: '26',
-    time: '11:00',
-    dayOfWeek: 'Domingo',
-    isAM: true,
-    is24Hs: false
+  const [datetime, setDatetime] = useState({
+    year: '',
+    month: '',
+    day: '',
+    time: '',
+    dayOfWeek: '',
+    isMorning: false
   });
-  const [language] = useState('es');
+  const [language] = useState('en');
+  const [wheather, setWheater] = useState({
+    location: { city: '', country: '', region: '' },
+    wheatherStatus: '',
+    celsius: ''
+  });
+  //const [isLoading, setLoading] = useState(true);
 
+  useEffect(() => {
+    fetch('http://ip-api.com/json/').then(res => res.json()).then((response) => {
+      console.log(response);
+      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${response.lat}&lon=${response.lon}&units=metric&APPID=ee054a87257ae80863574e6c33b0ab77`)
+      .then(res1 => res1.json()).then((responseWheather) => {
+      console.log(responseWheather);
+      //setLoading(false);
+      //set location and wheather
+
+      //set date and time
+      const date = moment();
+      setDatetime({
+        year: date.year().toString(),
+        month: date.format('MMMM'),
+        day: date.day().toString(),
+        time: date.format('hh:mm a'),
+        dayOfWeek: date.format('dddd'),
+        isMorning: false
+      });
+      //console.log(generateGreetings())
+      setWheater({
+        location: { city: response.city, country: response.country, region: response.regionName },
+        celsius: `${responseWheather.main.temp} °C`,
+        wheatherStatus: responseWheather.weather.map((element:any)=>element.description).join(',')
+      });
+      /*setInterval(()=>{
+        setDatetime({...datetime, time:moment().format('hh:mm a')});
+      },6000); */
+    })
+    })
+  }, []); // se ejecuta una sola vez al renderizar el componente.
+ 
   return (
     <div className="background">
       <div className="image-container">
           <img className="image" src={moon} alt="image" /> 
-          <p className="time">{getTime(datetime.time, datetime.isAM, datetime.is24Hs)}</p>
+          <p className="time">{datetime.time}</p>
       </div>
       <div className="info-container">
         <div className="info">
-          <h1>Ciudad de Guatemala</h1>
-          <h2>Mayormente Despejado</h2>
-          <h3>21°C</h3>
+          <h1>{  wheather.location.city }</h1>
+          <h2>{  !wheather.location.region.includes(wheather.location.city) ? wheather.location.region : '' }</h2>
+          <h2>{  !wheather.location.country.includes(wheather.location.city) ? wheather.location.country : ''}</h2>
+          <h3>{ wheather.celsius}</h3>
         </div>
         <div className="date">
           <p>{datetime.dayOfWeek}</p>
@@ -33,7 +72,7 @@ function App() {
         </div>
       </div>
     </div>
-  );
+  );  
 }
 
 function getDateByLanguage(day: string, month: string, language: string){
@@ -45,10 +84,23 @@ function getDateByLanguage(day: string, month: string, language: string){
 }
 
 function getTime(time: string, isAM: boolean, is24Hs: boolean) {
-  if (!is24Hs) {
-    return isAM ? time+' AM' : time +' PM';
-  }
   return time;
+}
+
+function generateGreetings(){
+  const currentHour = moment().hour();
+  if (currentHour >= 3 && currentHour < 12){
+      return "Good Morning";
+  } else if (currentHour >= 12 && currentHour < 15){
+      return "Good Afternoon";
+  }   else if (currentHour >= 15 && currentHour < 20){
+      return "Good Evening";
+  } else if (currentHour >= 20 && currentHour < 3){
+      return "Good Night";
+  } else {
+      return "Hello"
+  }
+
 }
 
 export default App;
